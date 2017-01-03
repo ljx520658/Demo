dd98f8e2a21c4a4c16682b56a8d5fa23c8a1d690
robin shine <robin@pmease.com>
2017-01-03 16:56:08 +0800

    Remove experimental code in Test.java

--- src/test/java/com/gitplex/symbolextractor/Test.java
+++ src/test/java/com/gitplex/symbolextractor/Test.java
@@ -1,18 +1,14 @@
 package com.gitplex.symbolextractor;
 
-import java.io.File;
-
-import org.apache.commons.io.Charsets;
-import org.sonar.javascript.parser.JavaScriptParser;
-import org.sonar.plugins.javascript.api.tree.Tree;
-
 public class Test {
 
 	@org.junit.Test
 	public void test() {
+		/*
 		JavaScriptParser parser = new JavaScriptParser(Charsets.UTF_8);
 		Tree tree = parser.parse(new File("w:\\temp\\test.js"));
 		System.out.println(tree.getClass());
+		*/
 	}
 
 }
